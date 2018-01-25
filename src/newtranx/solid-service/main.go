13a6d1cd46f80a878b404a45d09aa95fe9fc7992
luyi <luyi@newtranx.com>
2018-01-25 16:37:17 +0800

    impl

--- src/newtranx/solid-service/main.go
+++ src/newtranx/solid-service/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"newtranx/solid-service/server"
+	"os"
+	"fmt"
 )
 
 func main() {
@@ -11,5 +13,27 @@ func main() {
 	flag.IntVar(&server.Port, "p", 8080, "port")
 	flag.StringVar(&server.WorkPath, "w", "./work", "work path")
 	flag.Parse()
+	initWorkPath(server.WorkPath)
 	server.Start()
+}
+
+func initWorkPath(path string) {
+	ensurePath(path + "/" + server.SrcPath)
+	ensurePath(path + "/" + server.OutputPath)
+	ensurePath(path + "/" + server.ErrPath)
+}
+
+func ensurePath(path string) {
+	if info, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.MkdirAll(path, 0755)
+		if err != nil {
+			panic(err)
+		}
+	} else if err == nil {
+		if !info.IsDir() {
+			panic(fmt.Errorf("%s is not a folder", path))
+		}
+	} else {
+		panic(err)
+	}
 }
--- src/newtranx/solid-service/server/server.go
+++ src/newtranx/solid-service/server/server.go
@@ -4,6 +4,23 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"path/filepath"
+	"os"
+	"io"
+	"mime/multipart"
+	"github.com/fsnotify/fsnotify"
+	"context"
+	"io/ioutil"
+	"net/url"
+)
+
+const (
+	SrcPath       = "src"
+	OutputPath    = "out"
+	ErrPath       = "err"
+	FormFileField = "data"
 )
 
 type ServiceEndpoint struct {
@@ -11,9 +28,16 @@ type ServiceEndpoint struct {
 	Port     int
 	WorkPath string
 	srv      *http.Server
+	srcPath  string
+	outPath  string
+	errPath  string
 }
 
 func (s *ServiceEndpoint) Start() {
+	s.srcPath = s.WorkPath + "/" + SrcPath
+	s.outPath = s.WorkPath + "/" + OutputPath
+	s.errPath = s.WorkPath + "/" + ErrPath
+
 	router := gin.Default()
 
 	v1 := router.Group("v1")
@@ -26,5 +50,126 @@ func (s *ServiceEndpoint) Start() {
 }
 
 func (s *ServiceEndpoint) handleUpload(c *gin.Context) {
+	id, err := uuid.NewV4()
+	checkErr(err)
+	defer s.cleanup(id)
+	ctx, cancelCtx := newRequestContext(c, id)
+	defer cancelCtx()
+	log.Printf("<%s> begin", id.String())
+	uploaded, err := c.FormFile(FormFileField)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+	log.Printf("<%s> filename=%s, size=%d", id.String(), uploaded.Filename, uploaded.Size)
+	fileExt := filepath.Ext(uploaded.Filename)[1:] // remove the dot
+	baseName := uploaded.Filename[:len(uploaded.Filename)-(len(fileExt)+1)]
+	if fileExt != "pdf" {
+		c.String(http.StatusBadRequest, "not pdf")
+		return
+	}
+	oneTimeFilePath := s.srcPath + "/" + id.String() + ".pdf"
+	err = saveUploadedFile(uploaded, oneTimeFilePath)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	log.Printf("<%s> saved to %s", id.String(), oneTimeFilePath)
+	err = waitForConversionDone(oneTimeFilePath, ctx)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			// nothing
+		default:
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	log.Printf("<%s> conversion finished", id.String())
+	errLogPath := s.errPath + "/" + id.String() + ".log"
+	if _, err = os.Stat(errLogPath); os.IsNotExist(err) {
+		resultFileName := baseName + ".docx"
+		c.Header("Content-Disposition", "attachment; filename*=UTF-8''"+url.QueryEscape(resultFileName))
+		c.File(s.outPath + "/" + id.String() + ".docx")
+	} else {
+		errFile, err := os.Open(errLogPath)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		defer errFile.Close()
+		msg, _ := ioutil.ReadAll(errFile)
+		if msg != nil {
+			c.String(http.StatusInternalServerError, string(msg))
+		} else {
+			c.String(http.StatusInternalServerError, "unknown")
+		}
+	}
+	_ = baseName
+}
 
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func saveUploadedFile(uploaded *multipart.FileHeader, dst string) error {
+	srcFile, err := uploaded.Open()
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	dstFile, err := os.Create(dst)
+	defer dstFile.Close()
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func waitForConversionDone(oneTimeFilePath string, ctx context.Context) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
+	err = watcher.Add(oneTimeFilePath)
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Remove == fsnotify.Remove {
+				return nil
+			}
+		case err := <-watcher.Errors:
+			return err
+		case <-ctx.Done():
+			return fmt.Errorf("canceled")
+		}
+	}
+}
+
+func newRequestContext(c *gin.Context, id *uuid.UUID) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	notify := c.Writer.(http.CloseNotifier).CloseNotify()
+	go func() {
+		select {
+		case <-ctx.Done():
+		case <-notify:
+			log.Printf("<%s> canceled", id.String())
+			cancel()
+		}
+	}()
+	return ctx, cancel
+}
+
+func (s *ServiceEndpoint) cleanup(id *uuid.UUID) {
+	//TODO
 }
